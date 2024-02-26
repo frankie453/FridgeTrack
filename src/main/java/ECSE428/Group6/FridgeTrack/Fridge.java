b2bf209736ea4de1510cc189ca4b3c2e813c5137
haruhiko <zhanyue.zhang@mail.mcgill.ca>
2024-02-26 18:27:07 -0500

    add umple classes

---
+++ src/main/java/ECSE428/Group6/FridgeTrack/Fridge.java
@@ -0,0 +1,275 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.33.0.6934.a386b0a58 modeling language!*/
+
+
+import java.util.*;
+
+// line 2 "model.ump"
+// line 74 "model.ump"
+public class Fridge
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Fridge Attributes
+  private String description;
+
+  //Fridge Associations
+  private List<Recipe> recipes;
+  private List<Item> items;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Fridge(String aDescription)
+  {
+    description = aDescription;
+    recipes = new ArrayList<Recipe>();
+    items = new ArrayList<Item>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setDescription(String aDescription)
+  {
+    boolean wasSet = false;
+    description = aDescription;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getDescription()
+  {
+    return description;
+  }
+  /* Code from template association_GetMany */
+  public Recipe getRecipe(int index)
+  {
+    Recipe aRecipe = recipes.get(index);
+    return aRecipe;
+  }
+
+  public List<Recipe> getRecipes()
+  {
+    List<Recipe> newRecipes = Collections.unmodifiableList(recipes);
+    return newRecipes;
+  }
+
+  public int numberOfRecipes()
+  {
+    int number = recipes.size();
+    return number;
+  }
+
+  public boolean hasRecipes()
+  {
+    boolean has = recipes.size() > 0;
+    return has;
+  }
+
+  public int indexOfRecipe(Recipe aRecipe)
+  {
+    int index = recipes.indexOf(aRecipe);
+    return index;
+  }
+  /* Code from template association_GetMany */
+  public Item getItem(int index)
+  {
+    Item aItem = items.get(index);
+    return aItem;
+  }
+
+  public List<Item> getItems()
+  {
+    List<Item> newItems = Collections.unmodifiableList(items);
+    return newItems;
+  }
+
+  public int numberOfItems()
+  {
+    int number = items.size();
+    return number;
+  }
+
+  public boolean hasItems()
+  {
+    boolean has = items.size() > 0;
+    return has;
+  }
+
+  public int indexOfItem(Item aItem)
+  {
+    int index = items.indexOf(aItem);
+    return index;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfRecipes()
+  {
+    return 0;
+  }
+  /* Code from template association_AddManyToOne */
+  public Recipe addRecipe(String aName)
+  {
+    return new Recipe(aName, this);
+  }
+
+  public boolean addRecipe(Recipe aRecipe)
+  {
+    boolean wasAdded = false;
+    if (recipes.contains(aRecipe)) { return false; }
+    Fridge existingFridge = aRecipe.getFridge();
+    boolean isNewFridge = existingFridge != null && !this.equals(existingFridge);
+    if (isNewFridge)
+    {
+      aRecipe.setFridge(this);
+    }
+    else
+    {
+      recipes.add(aRecipe);
+    }
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeRecipe(Recipe aRecipe)
+  {
+    boolean wasRemoved = false;
+    //Unable to remove aRecipe, as it must always have a fridge
+    if (!this.equals(aRecipe.getFridge()))
+    {
+      recipes.remove(aRecipe);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addRecipeAt(Recipe aRecipe, int index)
+  {  
+    boolean wasAdded = false;
+    if(addRecipe(aRecipe))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfRecipes()) { index = numberOfRecipes() - 1; }
+      recipes.remove(aRecipe);
+      recipes.add(index, aRecipe);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveRecipeAt(Recipe aRecipe, int index)
+  {
+    boolean wasAdded = false;
+    if(recipes.contains(aRecipe))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfRecipes()) { index = numberOfRecipes() - 1; }
+      recipes.remove(aRecipe);
+      recipes.add(index, aRecipe);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addRecipeAt(aRecipe, index);
+    }
+    return wasAdded;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfItems()
+  {
+    return 0;
+  }
+  /* Code from template association_AddManyToOne */
+  public Item addItem(String aName, Unit aUnit, Recipe aRecipe, ItemList aItemList, ItemCategory aItemCategory)
+  {
+    return new Item(aName, aUnit, aRecipe, aItemList, aItemCategory, this);
+  }
+
+  public boolean addItem(Item aItem)
+  {
+    boolean wasAdded = false;
+    if (items.contains(aItem)) { return false; }
+    Fridge existingFridge = aItem.getFridge();
+    boolean isNewFridge = existingFridge != null && !this.equals(existingFridge);
+    if (isNewFridge)
+    {
+      aItem.setFridge(this);
+    }
+    else
+    {
+      items.add(aItem);
+    }
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeItem(Item aItem)
+  {
+    boolean wasRemoved = false;
+    //Unable to remove aItem, as it must always have a fridge
+    if (!this.equals(aItem.getFridge()))
+    {
+      items.remove(aItem);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addItemAt(Item aItem, int index)
+  {  
+    boolean wasAdded = false;
+    if(addItem(aItem))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfItems()) { index = numberOfItems() - 1; }
+      items.remove(aItem);
+      items.add(index, aItem);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveItemAt(Item aItem, int index)
+  {
+    boolean wasAdded = false;
+    if(items.contains(aItem))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfItems()) { index = numberOfItems() - 1; }
+      items.remove(aItem);
+      items.add(index, aItem);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addItemAt(aItem, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    for(int i=recipes.size(); i > 0; i--)
+    {
+      Recipe aRecipe = recipes.get(i - 1);
+      aRecipe.delete();
+    }
+    for(int i=items.size(); i > 0; i--)
+    {
+      Item aItem = items.get(i - 1);
+      aItem.delete();
+    }
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "description" + ":" + getDescription()+ "]";
+  }
+}
---
+++ src/main/java/ECSE428/Group6/FridgeTrack/ItemCategory.java
@@ -0,0 +1,168 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.33.0.6934.a386b0a58 modeling language!*/
+
+
+import java.util.*;
+
+// line 40 "model.ump"
+// line 106 "model.ump"
+public class ItemCategory
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //ItemCategory Attributes
+  private String name;
+
+  //ItemCategory Associations
+  private List<Item> items;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public ItemCategory(String aName)
+  {
+    name = aName;
+    items = new ArrayList<Item>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+  /* Code from template association_GetMany */
+  public Item getItem(int index)
+  {
+    Item aItem = items.get(index);
+    return aItem;
+  }
+
+  public List<Item> getItems()
+  {
+    List<Item> newItems = Collections.unmodifiableList(items);
+    return newItems;
+  }
+
+  public int numberOfItems()
+  {
+    int number = items.size();
+    return number;
+  }
+
+  public boolean hasItems()
+  {
+    boolean has = items.size() > 0;
+    return has;
+  }
+
+  public int indexOfItem(Item aItem)
+  {
+    int index = items.indexOf(aItem);
+    return index;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfItems()
+  {
+    return 0;
+  }
+  /* Code from template association_AddManyToOne */
+  public Item addItem(String aName, Unit aUnit, Recipe aRecipe, ItemList aItemList, Fridge aFridge)
+  {
+    return new Item(aName, aUnit, aRecipe, aItemList, this, aFridge);
+  }
+
+  public boolean addItem(Item aItem)
+  {
+    boolean wasAdded = false;
+    if (items.contains(aItem)) { return false; }
+    ItemCategory existingItemCategory = aItem.getItemCategory();
+    boolean isNewItemCategory = existingItemCategory != null && !this.equals(existingItemCategory);
+    if (isNewItemCategory)
+    {
+      aItem.setItemCategory(this);
+    }
+    else
+    {
+      items.add(aItem);
+    }
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeItem(Item aItem)
+  {
+    boolean wasRemoved = false;
+    //Unable to remove aItem, as it must always have a itemCategory
+    if (!this.equals(aItem.getItemCategory()))
+    {
+      items.remove(aItem);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addItemAt(Item aItem, int index)
+  {  
+    boolean wasAdded = false;
+    if(addItem(aItem))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfItems()) { index = numberOfItems() - 1; }
+      items.remove(aItem);
+      items.add(index, aItem);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveItemAt(Item aItem, int index)
+  {
+    boolean wasAdded = false;
+    if(items.contains(aItem))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfItems()) { index = numberOfItems() - 1; }
+      items.remove(aItem);
+      items.add(index, aItem);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addItemAt(aItem, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    while (items.size() > 0)
+    {
+      Item aItem = items.get(items.size() - 1);
+      aItem.delete();
+      items.remove(aItem);
+    }
+    
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "name" + ":" + getName()+ "]";
+  }
+}
